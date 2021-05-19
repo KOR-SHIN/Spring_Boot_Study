1f377f3e67773a1d6a76bed28c4df9bb2353f1ee
Shin KwangJin <shinkwang95@gmail.com>
2021-05-19 19:11:51 +0900

    [ADD] PostsApuController 추가 save, update, findById Method 추가 save 단위테스트 완료

---
+++ src/main/java/kr/co/h4tech/springboot/web/dao/PostsResponseDto.java
@@ -0,0 +1,23 @@
+package kr.co.h4tech.springboot.web.dao;
+
+import kr.co.h4tech.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+
+
+}
