b23c3ff90f875ccafb4e22a9995ffe742f1701fc
Shin KwangJin <shinkwang95@gmail.com>
2021-05-22 18:46:48 +0900

    [ADD] muschate plugin 설치 index.mustache 작성 및 테스트

--- src/main/java/kr/co/h4tech/springboot/Application.java
+++ src/main/java/kr/co/h4tech/springboot/Application.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  * SpringBootApplication Annotation이 있는 클래스부터 설정 파일을 읽기때문에
  * 항상 프로젝트 최상단에 위치해야 한다.
  *
- * @EnableJpaAuditing
+ * EnableJpaAuditing
  *  - JPA Auditing 활성화
  */
 @EnableJpaAuditing
