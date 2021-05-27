594042cdff1fe51a2cf42a41025bc9d6d8a24d29
Shin KwangJin <shinkwang95@gmail.com>
2021-05-27 11:41:05 +0900

    [ADD] 게시판 CRUD Api 등록 .gitignore에 oauth등록

--- src/main/java/kr/co/h4tech/springboot/service/posts/PostsService.java
+++ src/main/java/kr/co/h4tech/springboot/service/posts/PostsService.java
@@ -1,6 +1,7 @@
 package kr.co.h4tech.springboot.service.posts;
 
 import kr.co.h4tech.springboot.domain.posts.Posts;
+import kr.co.h4tech.springboot.domain.posts.PostsListResponseDto;
 import kr.co.h4tech.springboot.domain.posts.PostsRepository;
 import kr.co.h4tech.springboot.web.dao.PostsResponseDto;
 import kr.co.h4tech.springboot.web.dao.PostsSaveRequestDto;
@@ -8,6 +9,9 @@ import kr.co.h4tech.springboot.web.dao.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,24 @@ public class PostsService {
                 () -> new IllegalArgumentException("해당 게시글이 없습니다 id=" + id)
         );
         return new PostsResponseDto(entity);
+    }
+
+    /**
+     * readOnly = true 옵션을 사용할 경우
+     * 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선된다.
+     * 등록, 수정, 삭제 기능이 전혀 없는 서비스 메소드에서 사용하는 것을 추천한다.
+     */
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
--- src/test/java/kr/co/h4tech/springboot/web/HelloControllerTest.java
+++ src/test/java/kr/co/h4tech/springboot/web/HelloControllerTest.java
@@ -1,6 +1,5 @@
 package kr.co.h4tech.springboot.web;
 
-import kr.co.h4tech.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
